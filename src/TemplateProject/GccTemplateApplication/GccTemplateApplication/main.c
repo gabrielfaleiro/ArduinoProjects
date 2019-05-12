41518d0e0cbec206a215d49c46b2d7744ccfc05e
gfaleiro <faleirogabrielf@gmail.com>
2019-05-12 23:14:35 +0200

    First build

--- src/TemplateProject/GccTemplateApplication/GccTemplateApplication/main.c
+++ src/TemplateProject/GccTemplateApplication/GccTemplateApplication/main.c
@@ -5,14 +5,23 @@
  * Author : Gabriel Faleiro Rodríguez
  */ 
 
-#include <avr/io.h>
+#define F_CPU 16000000UL // 16 MHz
 
+#include <avr/io.h>
+#include <util/delay.h> // To use delay functions
 
 int main(void)
 {
+	/* Setup */
+	DDRB |= 0B00100000;
+	
     /* Replace with your application code */
     while (1) 
     {
+		PORTB |= 0B00100000;
+		_delay_ms(1000);
+		PORTB &= 0B11011111;
+		_delay_ms(10000);
     }
 }
 
